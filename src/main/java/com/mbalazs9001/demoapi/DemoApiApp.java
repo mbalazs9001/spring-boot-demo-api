8da490851889f715ca47b80d23cefcb98196f954
mbalazs9001 <mbalazs9001@gmail.com>
2018-09-25 22:28:02 +0200

    Added a test controller

---
+++ src/main/java/com/mbalazs9001/demoapi/DemoApiApp.java
@@ -0,0 +1,13 @@
+package com.mbalazs9001.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiApp.class, args);
+    }
+
+}
