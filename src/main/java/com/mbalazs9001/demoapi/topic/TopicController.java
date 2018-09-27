5ea47795f9d15d2c4c68c73846ca068269f94163
mbalazs9001 <mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.comgit config --global user.email mbalazs9001@gmail.com>
2018-09-27 11:09:39 +0200

    Added logic to get a single topic

--- src/main/java/com/mbalazs9001/demoapi/topic/TopicController.java
+++ src/main/java/com/mbalazs9001/demoapi/topic/TopicController.java
@@ -1,7 +1,9 @@
 package com.mbalazs9001.demoapi.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +17,10 @@ public class TopicController {
     @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
         return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
     }
 }
